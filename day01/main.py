3ace61f7852e31620798aa9adfc45b77df30df25
vegaal17 <vegaal17@uia.no>
2019-12-01 19:05:56 +0100

    part two

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,32 @@
 import math
 
-print(sum([(math.floor((int(n)/3))-2) for n in open("input.txt", "r")]))
+# one liner part 1
+#print(sum([(math.floor((int(n)/3))-2) for n in open("input.txt", "r")]))
+
+
+def part1():
+    f = open("input.txt", "r")
+    print(sum([(math.floor((int(n)/3))-2) for n in f]))
+    f.close()
+
+
+def part2():
+    f = open("input.txt", "r")
+    fuel = 0
+    for n in f:
+        fuel = fuel + recurse(int(n))
+    f.close()
+    print(f'Part2: Sum of fuel requirements: {fuel}')
+
+
+def recurse(fuel):
+    required = (math.floor((fuel / 3))-2)
+    if required > 0:
+        return required + recurse(required)
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
