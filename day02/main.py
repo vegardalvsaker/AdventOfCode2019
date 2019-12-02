ed8886539941fabcf560ff5dc62a1ea6a007eb7f
vegaal17 <vegaal17@uia.no>
2019-12-02 19:22:56 +0100

    solution day2

--- day02/main.py
+++ day02/main.py
@@ -1,15 +1,36 @@
-f = open("input.txt", "r")
-for l in f:
-    ints = list(map(int, l.split(",")))
-    ints[1] = 12
-    ints[2] = 2
+def program(ints):
     for i in range(0, len(ints), 4):
         if ints[i] is 1:
             ints[ints[i+3]] = ints[ints[i+1]] + ints[ints[i+2]]
         elif ints[i] is 2:
             ints[ints[i+3]] = ints[ints[i+1]] * ints[ints[i+2]]
         elif ints[i] is 99:
-            print("Program halted")
-            break
-    print(f'Value at positon 0: {ints[0]}')
-f.close()
+            return ints[0]
+
+
+def part1():
+    f = open("input.txt", "r")
+    for l in f:
+        ints = list(map(int, l.split(",")))
+        ints[1] = 12
+        ints[2] = 2
+        print(f'Value at positon 0: {program(ints)}')
+    f.close()
+
+
+def part2():
+    f = open("input.txt", "r")
+    for t in f:
+        for noun in range(0, 100):
+            for verb in range(0, 100):
+                ints = list(map(int, t.split(",")))
+                ints[1] = noun
+                ints[2] = verb
+                if program(ints) == 19690720:
+                    print(f'The answer is {100*noun+verb}')
+    f.close()
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
